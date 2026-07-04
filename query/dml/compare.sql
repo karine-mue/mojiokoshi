33005b9357dd40c37d0e6a6fc36e10ff8daf145c
ume_chan <ume_chan@local>
2026-07-04 21:43:18 +0900

    update stat

--- query/dml/compare.sql
+++ query/dml/compare.sql
@@ -1,4 +1,3 @@
--- 比較する
 select
   source_language,
   language_arg,
@@ -6,7 +5,8 @@ select
   round(avg(language_probability), 4) as avg_lang_prob,
   round(avg(elapsed_sec), 2) as avg_elapsed,
   round(avg(segment_count), 1) as avg_segments,
-  round(avg(transcript_chars), 1) as avg_chars
+  round(avg(transcript_chars), 1) as avg_chars,
+  count(*) as run_count
 from transcribe_runs
 where experiment_name = 'l0opback_lang_compare'
 group by
@@ -15,4 +15,5 @@ group by
   detected_language
 order by
   source_language,
-  language_arg;
+  language_arg,
+  detected_language;
