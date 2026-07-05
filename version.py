85cec1667671a51ddcc5f3c9655b8c5fcb2fbee0
karine <karine.mue80@gmail.com>
2026-07-05 13:57:59 +0900

    chore: add application version source

---
+++ version.py
@@ -0,0 +1,9 @@
+"""Application version for mojiokoshi.
+
+Keep this value aligned with Git tags and GitHub Release names.
+For example, ``__version__ = "0.1.0"`` corresponds to tag ``v0.1.0``.
+"""
+
+__version__ = "0.1.0"
+
+__all__ = ["__version__"]
